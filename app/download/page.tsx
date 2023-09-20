8d6e0980ca25229d13fade28c016c7dc3be2bf35
basilreji <basilreji3@gmail.com>
2023-09-20 16:49:32 +0530

    icon change

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Wrapper from "@/components/Wrapper";
 import "./style.scss";
 
 function Download() {
@@ -33,23 +34,29 @@ function Download() {
   };
 
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div id="ms-container">
-          <label htmlFor="ms-download">
-            <div className="ms-content">
-              <div className="ms-content-inside">
-                <input type="checkbox" id="ms-download" onClick={handleClick} />
-                <div className="ms-line-down-container">
-                  <div className="ms-line-down"></div>
+    <Wrapper>
+      <div className="container-fluid">
+        <div className="row">
+          <div id="ms-container">
+            <label htmlFor="ms-download">
+              <div className="ms-content">
+                <div className="ms-content-inside">
+                  <input
+                    type="checkbox"
+                    id="ms-download"
+                    onClick={handleClick}
+                  />
+                  <div className="ms-line-down-container">
+                    <div className="ms-line-down"></div>
+                  </div>
+                  <div className="ms-line-point"></div>
                 </div>
-                <div className="ms-line-point"></div>
               </div>
-            </div>
-          </label>
+            </label>
+          </div>
         </div>
       </div>
-    </div>
+    </Wrapper>
   );
 }
 
